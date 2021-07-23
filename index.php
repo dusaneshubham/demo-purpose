20e24d5cd2f0fd52d46b60b09e5acab72cbac9d6
dusaneshubham <56878960+dusaneshubham@users.noreply.github.com>
2021-07-23 22:42:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
-include "index.html";
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Hello, I am in hostel</h1>
+    <button onclick="myFunc()">Submit</button>
+</body>
+<script>
+    function myFunc() {
+        document.querySelector('h1').style.color = 'blue';
+    }
+</script>
+</html>
 ?>
