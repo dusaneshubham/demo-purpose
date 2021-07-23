54a518f5ac2972942ab58077af755cdf78e49e4c
dusaneshubham <56878960+dusaneshubham@users.noreply.github.com>
2021-07-23 23:32:13 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+$conn = mysqli_connect('remotemysql.com', 'ajLjW9uMW1', 'QlPLa4PrDU', 'ajLjW9uMW1') or die('Not connected');
+
+if (isset($_POST['submit'])) {
+    $user = $_POST['user'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $cpassword = $_POST['cpassword'];
+
+    $sql = "insert into signup(user,email,pass,cpass) values('$user', '$email', '$password', '$cpassword')";
+    $result = mysqli_query($conn, $sql);
+    if ($result) {
+        echo "<script>
+                alert('Submitted');
+              </script>";
+            header('location:index.php');
+    }
+    else {
+        echo "Not submitted";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="index.php" method="post">
+        <input type="text" placeholder="Enter user" name="user"><br><br>
+        <input type="email" placeholder="Enter email" name="email"><br><br>
+        <input type="password" placeholder="Enter password" name="password"><br><br>
+        <input type="password" placeholder="Enter cpassword" name="cpassword"><br><br>
+        <button type="submit" name="submit">Submit</button>
+    </form>
+</body>
+
+</html>
